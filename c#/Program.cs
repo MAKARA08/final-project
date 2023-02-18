1efe178ad3ebe66525917c4c37fe232c3e21cb4b
«nastya» <nastya@example.com>
2023-02-18 19:11:29 +0300

    второй коммит

--- c#/Program.cs
+++ c#/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿String[]array = new String [4];
+int counter = 0;
+Console.WriteLine("Введите значения для массива");
+for (int i = 0; i < array.Length; i++) {
+    array[i] = Console.ReadLine();
+    if (array[i].Length <= 3) {
+        counter++;
+        }
+}
+Console.WriteLine("Создан массив:");
+Console.WriteLine("[{0}]", string.Join(", ", array));
+Console.WriteLine(counter);
