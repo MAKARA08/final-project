0043f717ccf67d4f8591393dc68b5cbfc1272ddb
«nastya» <nastya@example.com>
2023-02-18 19:22:33 +0300

    Написала код

--- c#/Program.cs
+++ c#/Program.cs
@@ -1,12 +1,25 @@
-﻿String[]array = new String [4];
+﻿Console.WriteLine("Введите количество элементов в массиве:");
+String[]array = new String [Convert.ToInt32(Console.ReadLine())];
 int counter = 0;
-Console.WriteLine("Введите значения для массива");
+int j = 0;
+
+Console.WriteLine("Введите значения для массива:");
 for (int i = 0; i < array.Length; i++) {
     array[i] = Console.ReadLine();
     if (array[i].Length <= 3) {
         counter++;
-        }
+    }
 }
 Console.WriteLine("Создан массив:");
 Console.WriteLine("[{0}]", string.Join(", ", array));
-Console.WriteLine(counter);
+String[]mainArray = new String [counter];
+
+for (int i = 0; i < array.Length; i++) {
+    if (array[i].Length <= 3) {
+        mainArray[j] = array[i];
+        j++;
+    }
+} 
+
+Console.WriteLine("Массив элементов, где длина меньше 3-х символов:");
+Console.WriteLine("[{0}]", string.Join(", ", mainArray));
